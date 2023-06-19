01c2960a2eae110ae0c3cb4c83ff248c1026e6f7
Jonathan Budiman <65978976+JBudiman00@users.noreply.github.com>
2023-06-19 10:57:59 -0400

    Basic website layout structure
    FIrst commit to initialize website before working on individual pages

---
+++ frontend/src/app/components/navbar.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+
+export default function NavBar () {
+    const router = useRouter();
+    const tabElement = [
+        'flex',
+        'flex-col',
+        'w-32',
+        'justify-center',
+        'items-center',
+        'hover:bg-[#9F825B]'
+    ].join(' ');
+
+    return (
+        <div className="grid grid-cols-8 bg-[#DAC09B] h-16 sticky">
+            <div className="flex flex-row col-span-7 items-stretch">
+                <div 
+                    className={tabElement}
+                    onClick={() => router.push('/')}    
+                >
+                    <p className="text-lg">Home</p>
+                </div>
+                <div 
+                    className={tabElement}
+                    onClick={() => router.push('/nathan')}    
+                >
+                    <p className="text-lg">Nathan</p>
+                </div>
+                <div 
+                    className={tabElement}
+                    onClick={() => router.push('/chloe')}    
+                >
+                    <p className="text-lg">Chloe</p>
+                </div>
+                <div 
+                    className={tabElement}
+                    onClick={() => router.push('/us')}    
+                >
+                    <p className="text-lg">Chloe+Nathan</p>
+                </div>
+            </div>
+            <div className="flex flex-row col-span-1 justify-end">
+                <div 
+                    className={tabElement}
+                    onClick={() => router.push('/')}    
+                >
+                    <p className="text-lg ">Contact</p>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import { Raleway } from 'next/font/google'
+import NavBar from './components/navbar'
+import BottomBar from './components/bottombar'
+import './globals.css'
+
+const inter = Raleway({ weight: "500", subsets: ["latin"]})
+
+export const metadata = {
+  title: 'Blog Website',
+  description: 'Nathan + Chloe Blog Website',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        <BottomBar />
+      </body>
+    </html>
+  )
+}
